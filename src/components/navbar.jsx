57a9e266b05999cd93c9cc8c257435d7e2e0f8ef
BuildTools <unconfigured@null.spigotmc.org>
2022-01-10 22:37:11 +0000

    yes

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Stateless Functional Component
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
